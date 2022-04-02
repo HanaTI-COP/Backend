d118dda04c19f6138ed85d61022c3b7d80bded56
happ-in <gpqls304@naver.com>
2022-04-02 23:18:46 +0900

    Fix: Delete JpaRepository - 복합키로 인해 extends JpaRepository 할 시에 에러 발생 - 추후, 해당 Entity를 정의했을 때 구현

--- cop/src/main/java/com/hanati/cop/ProjectDetail/repository/ProjectDetailRepository.java
+++ cop/src/main/java/com/hanati/cop/ProjectDetail/repository/ProjectDetailRepository.java
@@ -3,5 +3,5 @@ package com.hanati.cop.ProjectDetail.repository;
 import com.hanati.cop.ProjectDetail.domain.ProjectDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectDetailRepository extends JpaRepository<ProjectDetail, Long> {
+public interface ProjectDetailRepository {
 }
